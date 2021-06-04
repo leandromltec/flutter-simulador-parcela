c981efd6fdf61a26d23907f9ec40c81f987a5b27
Leandro Milagre Loureiro <leandromltec@gmail.com>
2021-06-04 15:14:44 -0300

    Inclusão perfil página sobre

--- lib/app/views/about/about_screen.dart
+++ lib/app/views/about/about_screen.dart
@@ -1,10 +1,12 @@
 import 'dart:io';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_simulador_parcela/app/views/page_template.dart';
 import 'package:package_info/package_info.dart';
 
 //https://api.flutter.dev/flutter/dart-io/Platform-class.html
+//https://pub.dev/packages/package_info
 
 class AboutScreen extends StatefulWidget {
   final String? menuId;
@@ -19,12 +21,6 @@ class _AboutScreenState extends State<AboutScreen> {
   PackageInfo? package;
 
   @override
-  void initState() {
-    _packageInfo();
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return templatePage(
         selectedItemMenuId: widget.menuId,
@@ -33,40 +29,80 @@ class _AboutScreenState extends State<AboutScreen> {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              containerPage(textContent: "Simulador de Parcela Variável"),
-              containerPage(textContent: "App disponível para Android e IOS"),
+              Container(
+                  margin: EdgeInsets.only(top: 20, bottom: 20),
+                  width: 250,
+                  height: 60,
+                  decoration: BoxDecoration(
+                      color: Color(0xFF0084c7),
+                      borderRadius: BorderRadius.circular(20.0)),
+                  child: Center(
+                      child: Text(
+                    "Simulador de Parcela Variável",
+                    style: TextStyle(color: Colors.white),
+                  ))),
               containerPage(
-                  textContent:
-                      "Aplicativo desenvolvido com objetivo de prestação de contas com ANEEL, caso haja atraso em serviços."),
+                  widgetContent: Padding(
+                padding: const EdgeInsets.only(bottom: 10),
+                child: Text(
+                  "App disponível para Android e IOS",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              )),
               containerPage(
-                  textContent:
-                      "Irá atender profissionais da empresa Eletrobrás, que estarão atuando nas estações com os respectivos serviços."),
-              containerPage(textContent: "Tecnologia utilizada: Flutter"),
+                  widgetContent: Padding(
+                padding: const EdgeInsets.only(bottom: 10),
+                child: Text("Aplicativo desenvolvido com objetivo de prestação de contas com ANEEL," +
+                    " caso haja atraso em serviços. Atende profissionais da empresa Eletrobrás, " +
+                    "que estarão atuando nas estações com os respectivos serviços. \n" +
+                    "Algumas funcionalidades como câmera, tarefas e estação  foram adicionadas a nível de comprovação de conhecimento."),
+              )),
               containerPage(
-                  textContent: "Plataforma atual: " +
-                      Platform.operatingSystem.toString()),
-              if (package != null)
-                Container(
-                  child: Text("Versão: " + package!.version),
-                )
+                  widgetContent: Text("Tecnologia utilizada: Flutter")),
+              containerPage(
+                  widgetContent: Text("Plataforma atual: " +
+                      Platform.operatingSystem.toString())),
+              FutureBuilder<PackageInfo>(
+                  future: PackageInfo.fromPlatform(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done)
+                      return containerPage(
+                          widgetContent: Padding(
+                        padding: const EdgeInsets.only(bottom: 10),
+                        child: Text("Versão: ${snapshot.data!.version}"),
+                      ));
+                    else
+                      return CircularProgressIndicator();
+                  }),
+              containerPage(
+                  widgetContent: Text("Desenvolvido por:",
+                      style: TextStyle(fontWeight: FontWeight.bold))),
+              containerPage(widgetContent: Text("Leandro Loureiro")),
+              containerPage(
+                  widgetContent: Text("Analista Desenvolvedor Web e Mobile")),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton.icon(
+                      onPressed: () {},
+                      icon: Icon(Icons.data_usage),
+                      label: Text('Linkedin')),
+                  ElevatedButton.icon(
+                      onPressed: () {},
+                      icon: Icon(Icons.data_usage),
+                      label: Text('Github'))
+                ],
+              )
             ],
           ),
         ));
   }
 
-  Widget containerPage({required String textContent}) {
+  Widget containerPage({required Widget widgetContent}) {
     return Container(
-      width: MediaQuery.of(context).size.width,
-      margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
-      alignment: Alignment.center,
-      child: Text(textContent),
-    );
-  }
-
-  Future<void> _packageInfo() async {
-    final PackageInfo infoPackage = await PackageInfo.fromPlatform();
-    setState(() {
-      package = infoPackage;
-    });
+        width: MediaQuery.of(context).size.width,
+        margin: EdgeInsets.only(left: 30, right: 30, bottom: 5),
+        alignment: Alignment.center,
+        child: widgetContent);
   }
 }

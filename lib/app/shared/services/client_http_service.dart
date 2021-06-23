72561f2efecd65132734e97e462097578e54f8c5
Leandro Milagre Loureiro <leandromltec@gmail.com>
2021-06-23 14:32:48 -0300

    Criação da dialog de tratamento de erros

--- lib/app/shared/services/client_http_service.dart
+++ lib/app/shared/services/client_http_service.dart
@@ -9,14 +9,18 @@ import 'package:flutter_simulador_parcela/app/shared/constants/url_api_constants
 class ClientHttpService implements IClientHttpService {
   @override
   Future get(String endPoint) async {
-    var dataItemsAPI;
+    
+     var dataAPI;
+    try {
+      final response = await Dio().get(BASE_URL_API + endPoint);
+      if (response.statusCode != 200)
+        dataAPI = null;
+      else
+        dataAPI = response.data;
 
-    final response = await Dio().get(BASE_URL_API + endPoint);
-    if (response.statusCode != 200)
-      dataItemsAPI = null;
-    else
-      dataItemsAPI = response.data;
-
-    return dataItemsAPI;
+      return dataAPI;
+    } catch (e) {
+      
+    }
   }
 }
---
+++ test/get_management_test.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_simulador_parcela/app/controllers/pvi/pvi_controller.dart';
+import 'package:flutter_simulador_parcela/app/interfaces/client_http_interface.dart';
+import 'package:flutter_simulador_parcela/app/models/app/item_dropdown.dart';
+import 'package:flutter_simulador_parcela/app/shared/services/client_http_service.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+class ClientHttpMock {
+  final ClientHttpService clientHttp = new ClientHttpService();
+
+  dynamic getURL() async {
+    var json = await clientHttp.get('gerencia');
+    return json;
+  }
+}
+
+main() {
+  /*test('Erro Gerência', () async {
+    final controllerDropDown = Modular.get<ControllerPVI>();
+    List<ItemDropdDown> list =
+        await controllerDropDown.getListItensDropDownManagement();
+
+    expect(list, null);
+  });*/
+
+  test('OK API REST', () async {
+    final api = await ClientHttpMock().getURL();
+
+    expect(api, null);
+  });
+
+
+  
+}

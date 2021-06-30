bcb0cf663910e73e10b7998097cbb22869f8797b
Leandro Milagre Loureiro <appmongodbnodeflutter@gmail.com>
2021-06-29 21:32:17 -0300

    Url Router screen

--- lib/app/shared/constants/routes_screens_constants.dart
+++ lib/app/shared/constants/routes_screens_constants.dart
@@ -1,3 +1,10 @@
 /* Desenvolvido por Leandro M. Loureiro */
 /* GitHub https://github.com/leandromltec */
 /* Linkedin - https://www.linkedin.com/in/leandro-loureiro-dev/ */
+
+class RouteScreen {
+  final homeScreen = "/home";
+  final loginScreen = "/login";
+  final taskScreen = "/task";
+  
+}

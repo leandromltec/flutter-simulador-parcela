6aeda9afd2a09b6752babac228b87d8a37984b1c
Leandro Milagre Loureiro <appmongodbnodeflutter@gmail.com>
2021-06-29 21:37:17 -0300

    Correção das urls de rota

--- lib/app/shared/constants/routes_screens_constants.dart
+++ lib/app/shared/constants/routes_screens_constants.dart
@@ -6,5 +6,9 @@ class RouteScreen {
   final homeScreen = "/home";
   final loginScreen = "/login";
   final taskScreen = "/task";
-  
+  final pviScreen = "/pvi";
+  final pviResultScreen = "/pvi/resultado";
+  final cameraScreen = "/camera";
+  final cameraComponentScreen = "/camera/component";
+  final aboutScreen = "/about";
 }
